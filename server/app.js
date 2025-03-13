5a5631acc252db7ffe179b43bc0afdea1bd90f35
Anass Fadile <148713074+fadile-anass@users.noreply.github.com>
2025-03-13 00:29:37 +0000

    Update app.js

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use('/tool', toolRoutes);
 app.use('/type', typeRoutes);
 
 // Start Server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
