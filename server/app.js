a187d5533cfc026c0832d6eea6353d12a3ac753d
Anass Fadile <148713074+fadile-anass@users.noreply.github.com>
2025-03-15 23:41:55 +0000

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,8 +13,11 @@ const typeRoutes = require('./routes/typeRoutes');
 const authRoutes = require('./routes/authRoutes');  // Added auth routes import
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));app.use(express.json());
 
 // Routes Middleware
 app.use('/projects', projectRoutes);
