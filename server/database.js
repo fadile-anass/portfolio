0e167b2c24ce91113845392c7ea97e28ebaa666e
fadile-anass <anassfadile18@gmail.com>
2025-03-16 00:25:47 +0000

    try to fix deploy error 4

--- server/database.js
+++ server/database.js
@@ -4,7 +4,9 @@ const pool = mysql.createPool({
   uri: process.env.mysql_uri,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: process.env.NODE_ENV === 'production' ? {} : undefined
+
 });
 
 const promisePool = pool.promise();
