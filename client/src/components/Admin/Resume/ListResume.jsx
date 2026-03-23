ba526c2958554c7a824009f7b5a9e37033db319c
fadile-anass <anassfadile18@gmail.com>
2026-03-23 21:51:01 +0100

    add badge import

--- client/src/components/Admin/Resume/ListResume.jsx
+++ client/src/components/Admin/Resume/ListResume.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { Modal, Button, Card, Spinner, Alert, Container } from "react-bootstrap";
+import { Modal, Button, Card, Badge, Spinner, Alert, Container } from "react-bootstrap";
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
